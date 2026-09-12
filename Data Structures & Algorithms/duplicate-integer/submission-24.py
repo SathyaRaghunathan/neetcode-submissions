dfb0baf5508923dd9f3b9e3b7b63a8e553badf77
Sathya Raghunathan <36710665+SathyaRaghunathan@users.noreply.github.com>
2026-09-12 17:13:42 -0400

    Add: duplicate-integer - submission-24

---
+++ Data Structures & Algorithms/duplicate-integer/submission-24.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        visit = defaultdict(int)
+        for num in nums:
+            visit[num] = 1 + visit.get(num,0)
+        
+        for val in visit.values():
+            if val>1:
+                return True
+        return False
